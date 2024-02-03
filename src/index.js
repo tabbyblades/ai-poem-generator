c35fb72781f901218ede77dbe4c6ed103b2fe1b9
Tabby Blades <103293046+tabbyblades@users.noreply.github.com>
2024-02-03 17:33:19 +0900

    added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,30 +1,12 @@
 function displayPoem (event) {
     event.preventDefault();
 
-    let poemElement = document.querySelector("#poem");
-    poemElement.innerHTML = `These are your own words
-    <br/>
-    your way of noticing
-    and saying plainly
-    <br/>
-    of not turning away
-    <br/>
-    from hurt
-    <br/>
-    <br/>
-    you have offered them 
-    <br/>
-    to me I am only 
-    <br/>
-    giving them back 
-    <br />
-    <br/>
-    if only I could show you
-    <br/>
-    how very useless 
-    <br/>
-    they are not`;
-}
+    new Typewriter('#poem', {
+        strings: ["blah blah blah"],
+        autoStart: true,
+        delay: 'natural',
+        cursor: null,
+      }); }
 
 let poemForm = document.querySelector("#poem-form");
 poemForm.addEventListener("submit", displayPoem)
