85dc2683d195aa2d500c47d57b409f306015328b
Tabby Blades <103293046+tabbyblades@users.noreply.github.com>
2024-02-03 17:27:14 +0900

    created poemElement

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 function displayPoem (event) {
     event.preventDefault();
-    alert("generating poem");
-}
 
+    let poemElement = document.querySelector("#poem");
+    poemElement.innerHTML = `These are your own words
+    <br/>
+    your way of noticing
+    and saying plainly
+    <br/>
+    of not turning away
+    <br/>
+    from hurt
+    <br/>
+    <br/>
+    you have offered them 
+    <br/>
+    to me I am only 
+    <br/>
+    giving them back 
+    <br />
+    <br/>
+    if only I could show you
+    <br/>
+    how very useless 
+    <br/>
+    they are not`;
+}
 
 let poemForm = document.querySelector("#poem-form");
 poemForm.addEventListener("submit", displayPoem)
